ab3550b384ffd5902e62b0fae9bafcab1f8d2e19
junyoko1 <junyoko1@outlook.jp>
2026-01-24 23:06:33 +0900

    [ fix ] コメントをちょっと変更

--- Assets/Player/BillboardToCamera.cs
+++ Assets/Player/BillboardToCamera.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 
-/// <summary>
-/// 見た目を常にカメラ方向へ向ける
-/// </summary>
+
 public class BillboardToCamera : MonoBehaviour
 {
     [SerializeField] private bool lockY = true;
 
-    private Camera _camera;
-    private Transform _camTransform;
     private Transform _self;
+    private Transform _camTransform;
 
     private void Awake()
     {
@@ -27,26 +24,23 @@ public class BillboardToCamera : MonoBehaviour
     }
 
     /// <summary>
-    /// カメラの方向に向き保つ
+    /// カメラの方向を向く
     /// </summary>
     private void LateUpdate()
     {
         if (_camTransform == null) return;
 
         Vector3 camPos = _camTransform.position;
-
-        if (lockY)
-            camPos.y = _self.position.y;
-
+        if (lockY) camPos.y = _self.position.y;
+        
         _self.LookAt(camPos);
     }
 
     /// <summary>
-    /// 外部からカメラを注入する
+    /// カメラを設定する
     /// </summary>
     public void SetCamera(Camera cam)
     {
-        _camera = cam;
         _camTransform = cam != null ? cam.transform : null;
     }
 }
