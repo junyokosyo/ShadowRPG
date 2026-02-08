e28b2b705d6590cdf74db7c66a6f61ba9bdcaa32
junyoko1 <junyoko1@outlook.jp>
2026-02-08 18:09:29 +0900

    [ fix ]ビルボードに変更加えた

--- Assets/Player/BillboardToCamera.cs
+++ Assets/Player/BillboardToCamera.cs
@@ -1,44 +1,26 @@
 ﻿using UnityEngine;
 
-
 public class BillboardToCamera : MonoBehaviour
 {
     [SerializeField] private bool lockY = true;
 
-    private Transform _self;
     private Transform _camTransform;
 
-    private void Awake()
-    {
-        _self = transform;
-    }
-
-    private void OnEnable()
-    {
-        CameraEvents.CameraReady += SetCamera;
-    }
-
-    private void OnDisable()
-    {
-        CameraEvents.CameraReady -= SetCamera;
-    }
-
-    /// <summary>
-    /// カメラの方向を向く
-    /// </summary>
     private void LateUpdate()
     {
         if (_camTransform == null) return;
 
-        Vector3 camPos = _camTransform.position;
-        if (lockY) camPos.y = _self.position.y;
-        
-        _self.LookAt(camPos);
+        Vector3 dir = _camTransform.forward;
+
+        if (lockY)
+        {
+            dir.y = 0f;
+            if (dir.sqrMagnitude < 0.0001f) return;
+        }
+
+        transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
     }
 
-    /// <summary>
-    /// カメラを設定する
-    /// </summary>
     public void SetCamera(Camera cam)
     {
         _camTransform = cam != null ? cam.transform : null;
