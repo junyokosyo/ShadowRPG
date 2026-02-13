2803b558d0b51096a01e1b8e4abbccecbfc6cd24
junyoko1 <junyoko1@outlook.jp>
2026-02-13 21:26:07 +0900

    no message

--- Assets/Player/PlayerState.cs
+++ Assets/Player/PlayerState.cs
@@ -5,11 +5,17 @@ public class PlayerState : MonoBehaviour
     public bool IsMoving { get; private set; }
     public bool FacingRight { get; private set; } = true;
     public bool IsDiving { get; private set; }
+    public bool IsDetecctable { get;private set; }=true;
+
+
+    /// <summary>
+    /// プレイヤーに動いているかどうかを設定する
+    /// </summary>
+    /// 
     public void SetMoving(bool moving)
     {
         IsMoving = moving;
     }
-
     /// <summary>
     /// 左右の向きだけ更新する
     /// </summary>
@@ -20,8 +26,17 @@ public class PlayerState : MonoBehaviour
         else if (x < -0.01f)
             FacingRight = false;
     }
+    /// <summary>
+    /// プレイヤーがダイビングしているかどうかを設定する
+    /// </summary>
+    /// <param name="diving"></param>
     public void SetDiving(bool diving)
     {
         IsDiving = diving;
+        UpdateDetectable();
+    }
+    private void UpdateDetectable()
+    {
+        IsDetecctable = !IsDiving;
     }
 }
